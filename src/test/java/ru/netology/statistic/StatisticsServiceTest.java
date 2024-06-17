6c7c23f6d74d357baf3a52fa1892bc9db8c8afeb
Vitaliy V <vitaliyvilkov@gmail.com>
2024-06-17 19:02:40 +0400

    initial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMax2() {
         StatisticsService service = new StatisticsService();
